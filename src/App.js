335766f9dec8d87dd4ed2e32f53bbab4ff0e57c4
saad raja <shraja7@gmail.com>
2023-07-04 09:54:45 -0500

    add basic styling for the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
   return (
-    <div className="App">
-      <div>
-        <h1>Part 1</h1>
-      </div>
+    <div className="container">
+      <h2>Phonebook</h2>
+      <form className="form-container">
+        <div className="input-field">
+          Name: <input type="text" />
+        </div>
+        <div>
+          <button className="button" type="submit">
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {/* ... */}
     </div>
   );
-}
+};
 
 export default App;
