42c8d5bd255e5cbc1df74d81c4fff2cf979881be
saad raja <shraja7@gmail.com>
2023-07-04 10:42:02 -0500

    add search funtionality to show filtered persons and add styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [number, setNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -23,10 +27,11 @@ const App = () => {
       number: number,
     };
     //set persons state
-    setPersons(persons.concat(personObject));
+    // Update the persons state
+    setPersons([...persons, personObject]);
     //clear input
-    setNewName(" ");
-    setNumber(" ");
+    setNewName("");
+    setNumber("");
   };
 
   const handleNameInput = (event) => {
@@ -37,10 +42,33 @@ const App = () => {
   const handleNumberInput = (event) => {
     setNumber(event.target.value);
   };
-
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+  const filteredPersons = persons.filter((person) => {
+    const searchInput = search.toLowerCase();
+    return (
+      person.name.toLowerCase().includes(searchInput) ||
+      person.number.includes(searchInput)
+    );
+  });
   return (
     <div className="container">
       <h2>Phonebook</h2>
+      <div className="search-container">
+        <div className="input-field">
+          Search:
+          <input
+            type="text"
+            onChange={handleSearch}
+            value={search}
+            className="search-field"
+          />
+        </div>
+      </div>
+
+      <h2>Add New</h2>
+
       <form className="form-container" onSubmit={addPerson}>
         <div className="input-field">
           Name: <input type="text" onChange={handleNameInput} value={newName} />
@@ -62,16 +90,12 @@ const App = () => {
       </form>
       <h2>Contacts</h2>
       <div className="contacts-container">
-        <div className="contact-column">
-          {persons.map((person) => (
-            <p key={person.name}>{person.name}</p>
-          ))}
-        </div>
-        <div className="contact-column">
-          {persons.map((person) => (
-            <p key={person.name}>{person.number}</p>
-          ))}
-        </div>
+        {filteredPersons.map((person) => (
+          <div key={person.name} className="contact-box">
+            <p className="contact-name">{person.name}</p>
+            <p className="contact-number">{person.number}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
