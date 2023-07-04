9c286acf3bcf5cc1286b6d43c5ed2bca042a8120
saad raja <shraja7@gmail.com>
2023-07-04 10:02:10 -0500

    add a new person to the phonebook and clear input

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    //new person object
+    const personObject = {
+      name: newName,
+    };
+    //set persons state
+    setPersons(persons.concat(personObject));
+    //clear input
+    setNewName(" ");
+  };
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+    //clear input field only on the page
+  };
+
   return (
     <div className="container">
       <h2>Phonebook</h2>
-      <form className="form-container">
+      <form className="form-container" onSubmit={addPerson}>
         <div className="input-field">
-          Name: <input type="text" />
+          Name:{" "}
+          <input type="text" onChange={handleInputChange} value={newName} />
         </div>
         <div>
           <button className="button" type="submit">
@@ -17,8 +35,12 @@ const App = () => {
           </button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {/* ... */}
+      <h2>Contacts</h2>
+      {
+        persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        )) // key is used to identify each item in the list
+      }
     </div>
   );
 };
