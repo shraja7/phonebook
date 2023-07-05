9239912cb55889b8e4ed67f136233886e5c6f3c9
saad raja <shraja7@gmail.com>
2023-07-05 13:55:44 -0500

    update build

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 let persons = [
   {
@@ -92,7 +93,7 @@ app.post("/api/persons", (req, res) => {
 });
 
 //listen to port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
